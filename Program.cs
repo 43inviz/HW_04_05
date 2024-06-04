30dd8fa2bd7f50d0a59e80fefc8ce34bba913c59
Max Teplov <153938230+43inviz@users.noreply.github.com>
2024-06-04 17:25:58 +0300

    HW_2_04_06_2024

--- Program.cs
+++ Program.cs
@@ -19,12 +19,27 @@ namespace HW_03_06
 
             char num;
             MyNumToLine numToLine = new MyNumToLine();
-           for(int i = 0; i < 9;i++)
-           {
-                Console.Write("\nEnter number: ");
-                num = Console.ReadKey().KeyChar;
-                Console.Write($"\nResult: {numToLine.reversNumToLine(num)}");
-           }
+
+            Console.WriteLine("Converting numbers (0-9) from character to string\n");
+
+            try
+            {
+                do
+                {
+
+                    Console.WriteLine("\nEnter number: ");
+                    num = Console.ReadKey().KeyChar;
+                    Console.WriteLine($"\nResult: {numToLine.reversNumToLine(num)}");
+
+                    Console.WriteLine($"\nFor resume press [ENTER]\nFor Quit - press and HOLD[Esc]");
+                } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+          
                 
 
 
